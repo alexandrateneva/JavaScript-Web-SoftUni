b779cd3438a2dd23b4cb2ed1f6590916214c5841
Alexandra Teneva <alexteneva@gmail.com>
2018-05-30 14:24:49 +0300

    Add Introduction to Node.js and Node.js Web Server - exercises

--- ExpressJS Fundamentals/Node.js Web Server - Exercises/handlers/index.js
+++
@@ -1,8 +0,0 @@
-const homeHandler = require('./home');
-const filesHandler = require('./static-files');
-const allMoviesHandler = require('./allMovies');
-const movieDetailsHandler = require('./movieDetails');
-const addMovieHandler = require('./addMovie');
-const statusHandler = require('./status');
-
-module.exports = [homeHandler, filesHandler, allMoviesHandler, movieDetailsHandler, addMovieHandler, statusHandler];
