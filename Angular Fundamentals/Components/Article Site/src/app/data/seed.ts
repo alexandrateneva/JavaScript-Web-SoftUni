b5f3560291dbe656250216d6e9b89abf11f886e2
Alexandra Teneva <alexteneva@gmail.com>
2018-07-20 23:49:28 +0300

    Add Components - exercises

---
+++ Angular Fundamentals/Components/Article Site/src/app/data/seed.ts
@@ -0,0 +1,33 @@
+const data = [{
+    title: 'React',
+    description: 'In computing, React (also known as React.js or ReactJS) is a JavaScript library for building user interfaces. It is maintained by Facebook and a community of individual developers and companies.React can be used as a base in the development of single-page or mobile applications.',
+    author: 'Wikipedia',
+    imageUrl: 'http://builtwithreact.io/img/share-logo.jpg'
+  },
+  {
+    title: 'Angular',
+    description: 'Angular (commonly referred to as "Angular 2+" or "Angular v2 and above") is a TypeScript-based open-source front-end web application platform led by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS.',
+    author: 'Wikipedia',
+    imageUrl: 'https://ctl.s6img.com/society6/img/czhmCTfWHTn6-xbZNtmDFFD9voc/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/7f2d0aafee024f4ba8c91f9a60211ca5/~~/angular-js-developer-sticker-angularjs-javascript-framework-prints.jpg'
+  },
+  {
+    title: 'Express',
+    description: 'Express is a minimal and flexible Node.js web application framework that provides a robust set of features for web and mobile applications.',
+    author: 'expressjs.com',
+    imageUrl: 'https://edgetechacademy.edu/wp-content/uploads/2018/07/express_0.png'
+  },
+  {
+    title: 'NodeJS',
+    description: 'Node.js is an open-source, cross-platform JavaScript run-time environment that executes JavaScript code outside the browser. Historically, JavaScript was used primarily for client-side scripting, in which scripts written in JavaScript are embedded in a webpage`s HTML and run client-side by a JavaScript engine in the user`s web browser.',
+    author: 'Ryan Dahl',
+    imageUrl: 'https://ih1.redbubble.net/image.109336634.1604/flat,550x550,075,f.u1.jpg'
+  },
+  {
+    title: 'JavaScript',
+    description: 'JavaScript often abbreviated as JS, is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.',
+    author: 'Wikipedia',
+    imageUrl: 'https://cdn.instructables.com/FJM/7ZU4/IHZ4SYMW/FJM7ZU4IHZ4SYMW.LARGE.jpg?auto=webp&width=700'
+  }]
+  
+  export { data };
+  
